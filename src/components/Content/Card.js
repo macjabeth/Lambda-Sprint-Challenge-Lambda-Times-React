c0a7a430c58e717f5a7a47629a396cf07e8b6b78
macjabeth <macbethjonathan@gmail.com>
2019-02-22 21:23:27 +0000

    complete stretch: styled components throughout

--- src/components/Content/Card.js
+++ src/components/Content/Card.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const Card = ({headline, img, author}) => {
   return (
-    <div className="card">
-      <div className="headline">{headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <CardContainer>
+      <Headline>{headline}</Headline>
+      <Author>
+        <ImgWrapper>
           <img src={img} alt="" />
-        </div>
+        </ImgWrapper>
         <span>By {author}</span>
-      </div>
-    </div>
+      </Author>
+    </CardContainer>
   );
 };
 
@@ -21,5 +22,47 @@ Card.propTypes = {
   img: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired
 }
+
+const CardContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+
+const Author = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+
+  > span {
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+`;
+
+const ImgWrapper = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+
+  > img {
+    width: 40px;
+  }
+`;
 
 export default Card;
